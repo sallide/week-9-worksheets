125a5f3555b6a8ce7a7a82dbbb35f44f407eb38a
sallide <gvzh0213@leeds.ac.uk>
2025-12-05 10:35:24 +0000

    jhk

--- file_line_count.py
+++ file_line_count.py
@@ -1,9 +1,9 @@
 
-# Name: 
-# Username: 
+
+#Name: Saloni Pradhan
+#Student ID: 201829493
 
 import sys
-import pandas as pd
 
 # Show the number of non-empty lines in a text file
 # Usage: python file_line_count.py <filename>
@@ -11,10 +11,11 @@ import pandas as pd
 # You must use the error messages precisely as defied below.
 
 # filename is a command line argument 
-file_name = str(sys.argv[1])
 if len(sys.argv) != 2:
     sys.exit("Usage: python file_line_count.py <filename.txt>")
-#file_name = file_name[1]
+
+file_name = str(sys.argv[1])
+
 
 # Error message: "Usage: python file_line_count.py <filename.txt>"
 #f = open(file_name, "r")
@@ -24,18 +25,19 @@ try:
 
 # count number of lines that are not blank  
     number_of_lines = 0
-    number_of_lines = pd.read_csv(file_name)
- 
 
-    number_of_lines = (len(number_of_lines)+1)
+    f = open(file_name, "r")
+    lines = f.readlines()
 
-    print(f"{file_name} has {number_of_lines} lines")
-# success - report the number of lines
+    for line in lines:
+        if line.strip() != "":
+            number_of_lines +=1
 
+    sys.exit(f"{file_name} has {number_of_lines} lines")
 
 # Success message: "{file_name} has {number_of_lines} lines"
 
 # Test on the 'text_example.txt' file. The answer should be 4.
-except: 
+except FileNotFoundError: 
 
     sys.exit(f"File not found: python file_line_count.py {file_name}")
